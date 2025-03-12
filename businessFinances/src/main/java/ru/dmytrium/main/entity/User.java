d144d29678656c2b5fe6914180d199fa5b46eaaf
Dmitry1232321 <cuznetsov.dmitri2003@yandex.ru>
2025-03-13 02:48:17 +0300

    added security depency

--- businessFinances/src/main/java/ru/dmytrium/main/entity/User.java
+++ businessFinances/src/main/java/ru/dmytrium/main/entity/User.java
@@ -2,13 +2,17 @@ package ru.dmytrium.main.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "\"user\"")
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +31,13 @@ public class User {
     @Column(name = "created_at")
     private Date createdAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
 }
